8fcf5c33a4869b9d3f40dd118f4f942d970a5dbe
Alison Sipos <alison.sipos@icloud.com>
2020-11-12 19:05:12 -0700

    Created shell container for Navigation and Movie List

--- client/src/app.jsx
+++ client/src/app.jsx
@@ -1,9 +1,17 @@
+/* eslint-disable no-console */
 import React from 'react';
+import Container from 'react-bootstrap/Container';
+import NavMain from './components/NavMain.jsx';
+import AllMovies from './components/AllMovies.jsx';
 
-const App = () => (
-  <div>
-    Hello World!
-  </div>
-);
+const App = () => {
+  console.log('hello');
+  return (
+    <Container maxWidth="sm" className="main">
+      <NavMain />
+      <AllMovies />
+    </Container>
+  );
+};
 
 export default App;

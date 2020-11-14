36f8b44594b27d9833ff5ada83b2df9d622be675
Alison Sipos <alison.sipos@icloud.com>
2020-11-14 13:38:27 -0700

    Added classname, removed built in variant, added logo.

--- client/src/components/NavMain.jsx
+++ client/src/components/NavMain.jsx
@@ -2,18 +2,20 @@
 import React from 'react';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
-import Search from './Search';
+import Search from './Search.jsx';
 
 const NavMain = () => {
   console.log('nav');
   return (
-    <Navbar bg="dark" variant="dark">
-      <Navbar.Brand href="#home">FilmFavs</Navbar.Brand>
-      <Nav className="mr-auto">
+    <Navbar>
+      <Navbar.Brand href="#home">
+        <img src="https://film-favs.s3.us-east-2.amazonaws.com/FilmFavs_Logo.png" className="nav-logo" />
+      </Navbar.Brand>
+      {/* <Nav className=""> */}
         {/* <Nav.Link href="#home">Home</Nav.Link>
         <Nav.Link href="#features">Features</Nav.Link>
         <Nav.Link href="#pricing">Pricing</Nav.Link> */}
-      </Nav>
+      {/* </Nav> */}
       <Search />
     </Navbar>
   );

06246ab32dc5ab8e1525a1a7032f47f3d30dcbc4
Alison Sipos <alison.sipos@icloud.com>
2020-11-16 12:55:40 -0700

    Updated form to handle search input, outlined search functions.

--- client/src/components/Search.jsx
+++ client/src/components/Search.jsx
@@ -1,15 +1,46 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import Form from 'react-bootstrap/Form';
 import FormControl from 'react-bootstrap/FormControl';
 import Button from 'react-bootstrap/Button';
 
-const Search = () => {
+const Search = (setSearch, setResults, searchInput) => {
+  
+  const handleSearch = (e) => {
+    // if movie name is in all movies state
+    // return array of movies with full or partial match
+    // else
+    // return could not find movie gif
+    setSearch(String(e.target.value));
+  }
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setResults(searchInput);
+  }
+
   return (
     <Form inline>
-      <FormControl type="text" placeholder="Search" className="search-input" />
-      <Button className="search-button">Search</Button>
+        <Form.Control 
+          as="textarea"
+          placeholder="Search"
+          rows="1"
+          value={searchInput}
+          onChange={handleSearch}
+          className="search-input"
+          />
+        <Button 
+          className="search-button"
+        >
+        Search
+        </Button>
     </Form>
   )
+}
+
+Search.propTypes = {
+  setSearch: PropTypes.func.isRequired,
+  setResults: PropTypes.func.isRequired,
+  searchInput: PropTypes.string.isRequired,
 }
 
 export default Search;

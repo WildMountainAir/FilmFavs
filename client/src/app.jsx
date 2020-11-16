60bb2fea44cb1f8b91308500e521626b4451536c
Alison Sipos <alison.sipos@icloud.com>
2020-11-16 12:21:46 -0700

    Created new state for search and search results and passed to child components.

--- client/src/app.jsx
+++ client/src/app.jsx
@@ -1,14 +1,24 @@
 /* eslint-disable no-console */
-import React from 'react';
+import React, {useState} from 'react';
 import Container from 'react-bootstrap/Container';
 import NavMain from './components/NavMain.jsx';
 import AllMovies from './components/AllMovies.jsx';
 
 const App = () => {
+  // searchInput will hold the state of the Search Component
+  // setSearch will be passed as props to NavMain which will pass to Search
+  const [searchInput, setSearch] = useState('');
+  const [allFilms, setAllFilms] = useState([]);
+  const [searchResults, setResults] = useState([]);
+
   console.log('hello');
   return (
     <Container maxWidth="sm" className="main">
-      <NavMain />
+      <NavMain 
+        setSearch={setSearch}
+        setResults={setResults}
+        searchInput={searchInput}
+      />
       <AllMovies />
     </Container>
   );

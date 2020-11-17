7db81e64ec650d4e0bc1e6bf388154e781cf6a0a
Alison Sipos <alison.sipos@icloud.com>
2020-11-16 20:02:26 -0700

    Updated width of search area

--- client/src/components/Search.jsx
+++ client/src/components/Search.jsx
@@ -3,16 +3,21 @@ import PropTypes from 'prop-types';
 import Form from 'react-bootstrap/Form';
 import FormControl from 'react-bootstrap/FormControl';
 import Button from 'react-bootstrap/Button';
+import AllMovies from './AllMovies';
 
 const Search = (setSearch, setResults, searchInput) => {
   
-  const handleSearch = (e) => {
-    // if movie name is in all movies state
-    // return array of movies with full or partial match
-    // else
-    // return could not find movie gif
-    setSearch(String(e.target.value));
-  }
+  // const handleSearch = (e) => {
+  //   // if movie name is in all movies state
+  //   // return array of movies with full or partial match
+  //   // else
+  //   // return could not find movie gif
+  //   if (AllMovies.includes(e)) {
+  //     setSearch(String(e.target.value));
+  //   } else {
+  //     return 
+  //   }
+  // }
   const handleSubmit = (e) => {
     e.preventDefault();
     setResults(searchInput);
@@ -29,6 +34,7 @@ const Search = (setSearch, setResults, searchInput) => {
           className="search-input"
           />
         <Button 
+          onClick={handleSubmit}
           className="search-button"
         >
         Search

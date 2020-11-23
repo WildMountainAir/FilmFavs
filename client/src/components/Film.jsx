12dda16f9fae8871649db16f2ee4f3d55bafe106
Alison Sipos <alison.sipos@icloud.com>
2020-11-23 16:03:53 -0700

    Added displayInformation fn to an onClick event on the movie title.

--- client/src/components/Film.jsx
+++ client/src/components/Film.jsx
@@ -4,7 +4,7 @@ import React from 'react';
 var Film = (props) => { 
   return(
   <div className="movie-entry" >
-    <div className="movie-title" >{props.movie.title}
+    <div className="movie-title" onClick={displayInformation}>{props.movie.title}
     <button id="toggle-watched" className='movie-watched-button' onClick={(e) => {e.preventDefault(); props.watchHandler(props.movie.title)}}>Watched</button>
     </div>
   </div>
